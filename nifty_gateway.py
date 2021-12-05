5ba92649fd6491476752d57b4f1fb5a2e7a42ed5
Kyle McDonald <kyle@kylemcdonald.net>
2021-12-04 19:36:15 -0800

    update ng scraper

--- nifty_gateway.py
+++ nifty_gateway.py
@@ -15,11 +15,19 @@ def valid_hash(hash):
 def list_nifty_gateway(update=True, verbose=False):
     cache_fn = 'data/nifty-gateway-contracts.json'
 
-    if not update and os.path.exists(cache_fn):
+    cache = None
+    known_contracts = []
+    if os.path.exists(cache_fn):
         if verbose:
             print('Loading Nifty Gateway contracts from cache...')
         with open(cache_fn) as f:
-            return json.load(f)
+            cache = json.load(f)
+        known_contracts = list(cache.values())
+
+    if not update and cache is not None:
+        if verbose:
+            print('Returning Nifty Gateway contracts from cache...')
+        return cache
 
     drop_contracts = []
     if verbose:
@@ -37,26 +45,17 @@ def list_nifty_gateway(update=True, verbose=False):
     if verbose:
         print('Done.')
 
-    exhibition_contracts = []
-    if verbose:
-        print('Downloading from exhibition...')
-    url = f'https://api.niftygateway.com/exhibition/open/'
-    res = requests.get(url)
-    results = json.loads(res.content)
-    contracts = [e['contractAddress'] for e in results]
-    exhibition_contracts.extend(contracts)
-    if verbose:
-        print('total', len(exhibition_contracts))
-        print('Done.')
-
     filtered = {}
-    combined = set(exhibition_contracts + drop_contracts)
+    combined = set(known_contracts + drop_contracts)
     if verbose:
         print(f'Combined: total {len(combined)}')
     for i, hash in enumerate(combined):
         if not valid_hash(hash):
             continue
         filtered[f'Nifty Gateway/{i}'] = hash
+
+    if verbose:
+        print(f'Filtered: total {len(filtered)}')
 
     with open(cache_fn, 'w') as f:
         json.dump(filtered, f, indent=2)
