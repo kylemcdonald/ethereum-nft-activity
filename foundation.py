5a4fadc6dc37130720146ad68faff7e82d176379
Kyle McDonald <kyle@kylemcdonald.net>
2022-03-15 01:02:24 -0700

    ignore deployer when pulling foundation contracts

--- foundation.py
+++ foundation.py
@@ -9,7 +9,7 @@ etherscan = Etherscan(api_key, read_only=True)
 deployer = '0x3b612a5b49e025a6e4ba4ee4fb1ef46d13588059'
 transactions = etherscan.fetch_transactions_internal(deployer, verbose=True)
 contracts = [e['contractAddress'] for e in transactions]
-prefixed = prefix_contracts('Foundation', contracts)
+prefixed = prefix_contracts('Foundation', contracts, blocklist=[deployer])
 
 with open('data/foundation-contracts.json', 'w') as f:
     json.dump(prefixed, f, indent=2)
